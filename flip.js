545f60b4195707d560388cd1b11a6a5880af8180
Hawk Shannon <hawk.dillinger@gmail.com>
2019-10-04 14:10:07 -0400

    added card flip and circulating shperes

---
+++ flip.js
@@ -0,0 +1,5 @@
+var card = document.getElementById('card');
+              
+              document.getElementById('flip').addEventListener('click', function() {
+                  card.classList.toggle('flipped');
+              }, false);
