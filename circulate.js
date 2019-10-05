4467f603bdaec3994a5fef74c26232f0450b6a5e
Hawk Shannon <hawk.dillinger@gmail.com>
2019-10-05 12:50:34 -0400

    added a 4th sphere

--- circulate.js
+++ circulate.js
@@ -18,7 +18,7 @@ $(window).load(function() {
           width: -700,
           sizeAdjustment: 35,
           loop: true,
-          zIndexValues: [2, 2, 2, 2]
+          zIndexValues: [2, 2, 3, 3]
       })
   }
   
@@ -29,12 +29,24 @@ $(window).load(function() {
           width: -700,
           sizeAdjustment: 30,
           loop: true,
-          zIndexValues: [3, 3, 1, 1]
-      }).fadeIn();
+          zIndexValues: [3, 3, 2, 2]
+      })
   }
+  function startBallFour() {
+    $("#red-ball").circulate({
+        speed: 4000,
+        height: 140,
+        width: -700,
+        sizeAdjustment: 25,
+        loop: true,
+        zIndexValues: [4, 4, 1, 1]
+    }).fadeIn();
+}
+  
           
   startBallOne();
   setTimeout(startBallTwo, 2000);
   setTimeout(startBallThree, 4000);
+  setTimeout(startBallFour, 6000);
   
 });
